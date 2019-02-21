3a80b491c4bb7d19bb25b403a32790a82d60cce3
miaoying <miaoying@zhuojianchina.com>
2019-02-21 15:23:05 +0800

    dev; 添加通用类

--- common/src/main/java/com/peppa/exception/FeignErrorDecoder.java
+++ common/src/main/java/com/peppa/exception/FeignErrorDecoder.java
@@ -1,5 +1,62 @@
 package com.peppa.exception;
 
 
-public class FeignErrorDecoder {
+import com.alibaba.fastjson.JSONObject;
+import com.netflix.hystrix.exception.HystrixBadRequestException;
+import feign.Response;
+import feign.Util;
+import feign.codec.ErrorDecoder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class FeignErrorDecoder extends ErrorDecoder.Default {
+    @Override
+    public Exception decode(String methodKey, Response response) {
+        log.info("FeignErrorDecoder.decode(): method key: " + methodKey);
+        HystrixBadRequestException hbre = decodeBusinessException(response);
+        if (hbre != null) {
+            log.info("FeignErrorDecoder.decode(): receive business exception! return HystrixBadRequestException");
+            return hbre;
+        }
+        log.info("FeignErrorDecoder.decode(): return super.decode()");
+        return super.decode(methodKey, response);
+    }
+
+    private HystrixBadRequestException decodeBusinessException(Response response) {
+        String body = getBody(response);
+        if (body == null) {
+            return null;
+        }
+
+        JSONObject obj = JSONObject.parseObject(body);
+        Integer code = obj.getInteger("code");
+        if (code == null) {
+            return null;
+        }
+
+        String message = obj.getString("msg");
+        if (message == null) {
+            return null;
+        }
+
+        return new HystrixBadRequestException(body);
+    }
+
+    private String getBody(Response response) {
+        if (response == null || response.body() == null) {
+            return null;
+        }
+
+        try {
+            return Util.toString(response.body().asReader());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
 }
